c7d49529c0019ce7326d55798d6121b4af67a294
Sinchiguano <cesarsinchiguano@hotmail.es>
2018-09-09 21:13:43 +0100

    update

---
+++ devel/share/gennodejs/ros/hrwros_msgs/msg/_index.js
@@ -0,0 +1,42 @@
+
+"use strict";
+
+let CounterWithDelayFeedback = require('./CounterWithDelayFeedback.js');
+let TargetToolPoses = require('./TargetToolPoses.js');
+let CounterWithDelayActionResult = require('./CounterWithDelayActionResult.js');
+let CounterWithDelayResult = require('./CounterWithDelayResult.js');
+let CounterWithDelayActionFeedback = require('./CounterWithDelayActionFeedback.js');
+let CounterWithDelayAction = require('./CounterWithDelayAction.js');
+let SensorInformation = require('./SensorInformation.js');
+let RobotTrajectories = require('./RobotTrajectories.js');
+let ObjectDetection = require('./ObjectDetection.js');
+let CounterWithDelayGoal = require('./CounterWithDelayGoal.js');
+let CounterWithDelayActionGoal = require('./CounterWithDelayActionGoal.js');
+let CounterWithDelayFeedback = require('./CounterWithDelayFeedback.js');
+let CounterWithDelayActionResult = require('./CounterWithDelayActionResult.js');
+let CounterWithDelayResult = require('./CounterWithDelayResult.js');
+let CounterWithDelayActionFeedback = require('./CounterWithDelayActionFeedback.js');
+let CounterWithDelayAction = require('./CounterWithDelayAction.js');
+let CounterWithDelayGoal = require('./CounterWithDelayGoal.js');
+let CounterWithDelayActionGoal = require('./CounterWithDelayActionGoal.js');
+
+module.exports = {
+  CounterWithDelayFeedback: CounterWithDelayFeedback,
+  TargetToolPoses: TargetToolPoses,
+  CounterWithDelayActionResult: CounterWithDelayActionResult,
+  CounterWithDelayResult: CounterWithDelayResult,
+  CounterWithDelayActionFeedback: CounterWithDelayActionFeedback,
+  CounterWithDelayAction: CounterWithDelayAction,
+  SensorInformation: SensorInformation,
+  RobotTrajectories: RobotTrajectories,
+  ObjectDetection: ObjectDetection,
+  CounterWithDelayGoal: CounterWithDelayGoal,
+  CounterWithDelayActionGoal: CounterWithDelayActionGoal,
+  CounterWithDelayFeedback: CounterWithDelayFeedback,
+  CounterWithDelayActionResult: CounterWithDelayActionResult,
+  CounterWithDelayResult: CounterWithDelayResult,
+  CounterWithDelayActionFeedback: CounterWithDelayActionFeedback,
+  CounterWithDelayAction: CounterWithDelayAction,
+  CounterWithDelayGoal: CounterWithDelayGoal,
+  CounterWithDelayActionGoal: CounterWithDelayActionGoal,
+};
---
+++ devel/share/hrwros_msgs/cmake/hrwros_msgs-msg-paths.cmake
@@ -0,0 +1,4 @@
+# generated from genmsg/cmake/pkg-msg-paths.cmake.develspace.in
+
+set(hrwros_msgs_MSG_INCLUDE_DIRS "/home/casch/hrwros_ws/src/hrwros/hrwros_msgs/msg;/home/casch/hrwros_ws/devel/share/hrwros_msgs/msg")
+set(hrwros_msgs_MSG_DEPENDENCIES std_msgs;geometry_msgs;trajectory_msgs;moveit_msgs;actionlib_msgs)
